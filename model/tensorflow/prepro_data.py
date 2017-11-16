1ad09ecac81b27e5ddbf4c47e7d068f9880b2128
Ubuntu <ubuntu@ip-172-31-93-45.ec2.internal>
2017-11-16 05:40:02 +0000

    path updates

--- model/tensorflow/prepro_data.py
+++ model/tensorflow/prepro_data.py
@@ -6,7 +6,7 @@ import scipy.misc
 def createH5(params):
 
 	# create output h5 file
-	output_h5 = '%s_%d_%s.h5' %(params['name'], params['img_resize'], params['split'])
+	output_h5 = '../data/%s_%d_%s.h5' %(params['name'], params['img_resize'], params['split'])
 	f_h5 = h5py.File(output_h5, "w")
 
 	# read data info from lists
@@ -47,7 +47,7 @@ if __name__=='__main__':
 		'name': 'miniplaces',
 		'split': 'train',
 		'img_resize': 256,
-		'data_root': '../../data/images/',	# MODIFY PATH ACCORDINGLY
+		'data_root': '../data/images/',	# MODIFY PATH ACCORDINGLY
     		'data_list': '../../data/train.txt'
 	}
 
@@ -55,7 +55,7 @@ if __name__=='__main__':
 		'name': 'miniplaces',
 		'split': 'val',
 		'img_resize': 256,
-		'data_root': '../../data/images/',	# MODIFY PATH ACCORDINGLY
+		'data_root': '../data/images/',	# MODIFY PATH ACCORDINGLY
     		'data_list': '../../data/val.txt'
 	}
 
